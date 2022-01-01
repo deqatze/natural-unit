0752e9ef205d728b501aef687dd7c42bdc362723
deqatze <72379909+deqatze@users.noreply.github.com>
2022-01-01 15:05:06 +0700

    Update units.py

--- units.py
+++ units.py
@@ -1,11 +1,11 @@
-pi=3.141592535
+pi=3.1415926535
 
 c=299792458
 h=6.62607015*10**-34
 G=6.6743*10**-11
 kB=1.38065*10**-23
 e0=8.8541878*10**-12
-K=638
+Kcd=683
 
 def x(a):
     global h,e0
@@ -22,10 +22,12 @@ def x(a):
 m=(h*G/c**3)**0.5
 kg=(h*c/G)**0.5
 s=(h*G/c**5)**0.5
+hr=60*s
+Hz=1/s
 K=(h*c**5/(G*kB**2))**0.5
 C=(h*c*e0)**0.5
 A=C/s
-lm=K*c**5/G
+lm=Kcd*c**5/G
 lx=lm/m**2
 N=kg*m/s**2
 Pa=N/m**2
@@ -35,6 +37,10 @@ V=J/C
 F=C/V
 ohm=h/C**2
 Wb=h/C
+T=Wb/m**2
+H=Wb/A
+def C(x):
+    return x-273.15
 
 print('natural   >>    SI        *')
 print(' SI       >>    natural   /')
